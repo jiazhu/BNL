15aa5489493d6da90dd1cff83e375586909d6587
Jia Zhu <jiazhu@Jias-MacBook-Air.local>
2011-08-06 00:18:36 +1000

    Update annoation

--- Map.h
+++ Map.h
@@ -5,13 +5,23 @@
 //  Created by Jia Zhu on 5/08/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
-
+#import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
+
+@interface AddressAnnotation : NSObject <MKAnnotation>
+{
+    CLLocationCoordinate2D coordinate;
+    
+    NSString *title;
+    NSString *subtitle;
+}
+@end
+
 @interface Map : UIViewController{
     
     IBOutlet MKMapView *mapView;    
-    
-    
+    AddressAnnotation *addAnnotation;    
 }
+- (IBAction)showAddress;
 
 @end
--- Map.m
+++ Map.m
@@ -7,9 +7,30 @@
 //
 
 #import "Map.h"
+@implementation AddressAnnotation
+
+@synthesize coordinate;
+
+- (NSString *)subtitle
+{   
+    //please modify to retrieve subtitle from database
+    return @"Subtitle";
+}
+- (NSString *)title
+{
+    return @"Title";
+}
+
+-(id)initWithCoordinate:(CLLocationCoordinate2D) c
+{
+    coordinate = c;
+    NSLog(@"%f,$f", c.latitude,c.longitude);
+    return self;
+}
+
+@end
 
 @implementation Map
-
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -30,25 +51,71 @@
 #pragma mark - View lifecycle
 
 /*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView
-{
-}
-*/
-
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-}
-*/
+ // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+ - (void)viewDidLoad
+ {
+ [super viewDidLoad];
+ }
+ */
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self showAddress];
+}
+
+- (IBAction) showAddress
+{
+    
+    MKCoordinateRegion region;
+    MKCoordinateSpan span;
+    span.latitudeDelta=0.2;
+    span.longitudeDelta=0.2;
+    
+    CLLocationCoordinate2D location = mapView.userLocation.coordinate;
+    //please modify to retrieve location from database
+    location.latitude = -27.520893 ;
+    location.longitude = 153.027785;
+    region.span=span;
+    region.center=location;
+    
+    if(addAnnotation != nil)
+    {
+        [mapView removeAnnotation:addAnnotation];
+        [addAnnotation release];
+        addAnnotation = nil;
+    }
+    
+    addAnnotation = [[AddressAnnotation alloc] initWithCoordinate:location];
+    [mapView addAnnotation:addAnnotation];
+    
+    [mapView setRegion:region animated:TRUE];
+    [mapView regionThatFits:region];
+    //[mapView selectAnnotation:mLodgeAnnotation animated:YES];
+    
+}
+
+- (MKAnnotationView *) mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>) annotation
+{
+    MKPinAnnotationView *annView=[[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"currentloc"];
+    annView.pinColor = MKPinAnnotationColorGreen;
+    annView.animatesDrop=TRUE;
+    annView.canShowCallout = YES;
+    annView.calloutOffset = CGPointMake(-5, 5);
+    return annView;
+}
+
+
+- (void)dealloc
+{
+    [super dealloc];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
