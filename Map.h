48ccb74b291ceb9095445f09bf8df82bd60fe4d8
Jia Zhu <jiazhu@Jias-MacBook-Air.local>
2011-08-05 23:02:33 +1000

    Added map view.

--- Map.h
+++ Map.h
@@ -6,9 +6,11 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-
-
+#import <MapKit/MapKit.h>
 @interface Map : UIViewController{
+    
+    IBOutlet MKMapView *mapView;    
+    
     
 }
 
