3d7dbe2d5c7fc7f8cf4d6cdd04bad930d947225d
Jia Zhu <jiazhu@Jias-MacBook-Air.local>
2011-08-05 22:27:20 +1000

    First commit

---
+++ ContactUs.h
@@ -0,0 +1,15 @@
+//
+//  ContactUs.h
+//  BNL
+//
+//  Created by Jia Zhu on 5/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface ContactUs : UIViewController{
+    
+}
+
+@end
---
+++ Results.h
@@ -0,0 +1,15 @@
+//
+//  Results.h
+//  BNL
+//
+//  Created by Jia Zhu on 5/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface Results : UIViewController{
+    
+}
+
+@end
